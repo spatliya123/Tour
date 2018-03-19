34e7772a4425c6e35fc0a0a5483cfe218ab388c1
s978410204@gmail.com <Shanti@13>
2018-03-20 02:44:12 +0530

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul>
+				<li><a class="facebook" href="https://www.facebook.com/shantilal13.love/photos_all"><span>Facebook</span></a></li>
+				<li><a class="twitter" href="https://www.facebook.com/shantilal13.love/photos_all"><span>Twitter</span></a></li>
+				<li><a cla="googleplus" href="https://www.facebook.com/shantilal13.love/photos_all"><span>Google+</span></a></li>
+				
+
+				
+			</ul>
+		</div>
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2018  Copyright <a href="https://www.facebook.com/shantilal13.love">Shantilal Patliya</a></p>
+	</div>
+</div>
